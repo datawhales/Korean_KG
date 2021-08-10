548d8a714eb554a5a99c7b92f2e585b802824fa6
datawhales <datawhales@gmail.com>
2021-08-10 17:19:19 +0900

    Add tokenize function

--- code/utils.py
+++ code/utils.py
@@ -17,6 +17,7 @@ class EntityPosMarker:
     """
     def __init__(self, args=None):
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-cased')
+        self.err = 0
         self.args = args
     
     def tokenize(self, sentence, subj_pos_range, obj_pos_range):
@@ -29,8 +30,10 @@ class EntityPosMarker:
 
         Returns:
             tokenized_input_ids: BERT-input ids
-            subj_token_start: subject entity marker 시작 위치 인덱스
-            obj_token_start: object entity marker 시작 위치 인덱스
+            subj_marker_start: subject entity marker 시작 위치 인덱스
+            subj_marker_end: subject entity marker 끝 위치 인덱스
+            obj_marker_start: object entity marker 시작 위치 인덱스
+            obj_marker_end: object entity marker 끝 위치 인덱스
 
         Example:
             sentence: "한국은 동아시아의 한반도에 위치하고 있다."
@@ -44,7 +47,8 @@ class EntityPosMarker:
             1. tokenizer.tokenize(sentence)
                 tokens = ['한국', '##은', '동', '##아', '##시아', '##의', '한', '##반', '##도에', '위', '##치', '##하고', '있다', '.']
             2. special token 추가
-                "[CLS] [unused1] 한국 [unused2] ##은 [unused3] 동 ##아 ##시아 [unused4] ##의 한 ##반 ##도에 위 ##치 ##하고 있다 . [SEP]"
+                tokenized_sentence = ['[CLS]', '[unused1]', '한국', '[unused2]', '##은', '[unused3]', '동', '##아', '##시아', 
+                                    '[unused4]', '##의', '한', '##반', '##도에', '위', '##치', '##하고', '있다', '.', '[SEP]']
             3. tokenized sentence를 BERT-input ids로 변환 및 entities 위치 찾아 return
                 [101, 102]
         """
@@ -58,6 +62,7 @@ class EntityPosMarker:
         # 1. tokenizer.tokenize(sentence)
         tokens = self.tokenizer.tokenize(sentence)
 
+        # subj, obj token 위치 찾기
         for i, token in enumerate(tokens):
             if token[0] == subj_name[0]:
                 subj_start_idx.append(i)
@@ -78,12 +83,13 @@ class EntityPosMarker:
                 subj_cand = " ".join(tmp_tokens)
                 subj_cand = re.sub(" ##", "", subj_cand)
                 if subj_cand == subj_name:
-                    subj_token_start, subj_token_end = i, j
+                    subj_token_start, subj_token_end = i, j+1
                     subj_flag = True
                     break
             if subj_flag:
                 break
-        
+
+        # obj token idx
         obj_flag = False
         for i in obj_start_idx:
             for j in obj_end_idx:
@@ -92,13 +98,53 @@ class EntityPosMarker:
                 obj_cand = " ".join(tmp_tokens)
                 obj_cand = re.sub(" ##", "", obj_cand)
                 if obj_cand == obj_name:
-                    obj_token_start, obj_token_end = i, j
+                    obj_token_start, obj_token_end = i, j+1
                     obj_flag = True
                     break
             if obj_flag:
                 break
-        
 
+        # subj_tokens = tokens[subj_token_start:subj_token_end]
+        # obj_tokens = tokens[obj_token_start:obj_token_end]
+
+        # 2. special token 추가
+        tokenized_sentence = []
+        for i, token in enumerate(tokens):
+            if i == subj_token_start:
+                tokenized_sentence.append("[unused1]")
+            elif i == obj_token_start:
+                tokenized_sentence.append("[unused3]")
+
+            if i == subj_token_end:
+                tokenized_sentence.append("[unused2]")
+            elif i == obj_token_end:
+                tokenized_sentence.append("[unused4]")
+
+            tokenized_sentence.append(token)
+
+        tokenized_sentence = ["[CLS]"] + tokenized_sentence + ["[SEP]"]
+
+        # 3. entity marker token 위치 찾기
+        try:
+            subj_marker_start = tokenized_sentence.index("[unused1]")
+            subj_marker_end = tokenized_sentence.index("[unused2]")
+        except:
+            self.err += 1
+            subj_marker_start = 0
+            subj_marker_end = 2
+        
+        try:
+            obj_marker_start = tokenized_sentence.index("[unused3]")
+            obj_marker_end = tokenized_sentence.index("[unused4]")
+        except:
+            self.err += 1
+            obj_marker_start = 0
+            obj_marker_end = 2
+
+        tokenized_input_ids = self.tokenizer.convert_tokens_to_ids(tokenized_sentence)
+        
+        return tokenized_input_ids, subj_marker_start, subj_marker_end, obj_marker_start, obj_marker_end
+        
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--filepath", dest="filepath", type=str, help="filepath")
