bb6ed1f1328e119d50b56204a4420bc9a9c28bdb
datawhales <datawhales@gmail.com>
2021-10-08 13:55:15 +0900

    Modified codes

--- code/dataset.py
+++ code/dataset.py
@@ -35,5 +35,34 @@ class REDataset(Dataset):
         self.obj_marker_start = np.zeros(total_data, dtype=int)
         self.obj_marker_end = np.zeros(total_data, dtype=int)
         self.label = np.zeros(total_data, dtype=int)
-    
-        
+
+        for i, item in enumerate(data):
+            self.label[i] = rel2id[item['description']]  # description = 한국어 relation
+
+            sentence = item['sentence']
+            subj_pos_range = item['subj']['pos']
+            obj_pos_range = item['obj']['pos']
+
+            tokenized_input_ids, subj_marker_start, subj_marker_end, obj_marker_start, obj_marker_end = marker.tokenize(sentence=sentence, subj_pos_range = subj_pos_range, obj_pos_range=obj_pos_range)
+
+            length = min(len(tokenized_input_ids), args.max_length)
+            self.tokenized_input_ids[i][0:length] = tokenized_input_ids[0:length]
+            self.mask[i][0:length] = 1
+            self.subj_marker_start[i] = min(subj_marker_start, args.max_length - 1)
+            self.subj_marker_end[i] = min(subj_marker_end, args.max_length - 1)
+            self.obj_marker_start[i] = min(obj_marker_start, args.max_length - 1)
+            self.obj_marker_end[i] = min(obj_marker_end, args.max_length - 1)
+            
+    def __len__(self):
+        return len(self.tokenized_input_ids)
+
+    def __getitem__(self, idx):
+        tokenized_input_ids = self.tokenized_input_ids[idx]
+        mask = self.mask[idx]
+        subj_marker_start = self.subj_marker_start[idx]
+        subj_marker_end = self.subj_marker_end[idx]
+        obj_marker_start = self.obj_marker_start[idx]
+        obj_marker_end = self.obj_marker_end[idx]
+        label = self.label[idx]
+
+        return tokenized_input_ids, mask, subj_marker_start, subj_marker_end, obj_marker_start, obj_marker_end, label
